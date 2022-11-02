67bdc6733f619831dfecc4b7c30973ee7354d087
Tom Watson <tom_w@fastmail.com>
2022-11-02 14:18:48 +1100

    Use DMA stream writer, instead of immutable, and swap to logs instead of print statements

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use futures_lite::AsyncWriteExt;
-use glommio::{LocalExecutorBuilder, Placement};
+use glommio::{LocalExecutorBuilder, Placement, io::DmaStreamWriterBuilder, defer};
 use itertools::Itertools;
 use queries::{IndexData, PersistentQuery};
 use search::{Searcher, TextSource};
-use std::{error, net::SocketAddr, path::Path};
-use tachyonix::{self};
+use tracing::{Level, event};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use std::{error, net::SocketAddr, path::{PathBuf}};
+use tachyonix;
 
 mod data_source;
 mod errors;
@@ -15,27 +18,37 @@ mod storage;
 
 use crate::server::server_runtime;
 
+const DATA_PATH: &str = "output_data";
+
 fn main() -> Result<(), Box<dyn error::Error>> {
+    tracing_subscriber::registry()
+    .with(tracing_subscriber::EnvFilter::new(
+        std::env::var("RUST_LOG")
+            .unwrap_or_else(|_| "tarkine=debug,tower_http=debug".into()),
+    ))
+    .with(tracing_subscriber::fmt::layer())
+    .init();
     let core_count = std::thread::available_parallelism()?;
-    dbg!(&core_count);
-    let bind_addr = SocketAddr::from(([127, 0, 0, 1], 8765));
+    event!(Level::INFO, core_count);
+    let bind_addr = SocketAddr::from(([127, 0, 0, 1], 8766));
 
     let (write_map, read_map) = flashmap::with_capacity(1000);
     let (send_chan, recv_chan) = tachyonix::channel(1024);
-
+    event!(Level::INFO, message="Starting API server thread");
     let server_threads =
         std::thread::spawn(move || server_runtime(bind_addr, write_map, send_chan));
-    let shard1 = QueryShard {
-        id: rand::random(),
-        inner: read_map,
-        engine: Searcher::new(),
-    };
+        let shard1 = QueryShard {
+            id: rand::random(),
+            inner: read_map,
+            engine: Searcher::new(),
+        };
+    event!(Level::INFO, message="Starting Indexing Thread");
     let processor_thread = LocalExecutorBuilder::new(Placement::Fixed(1))
         .spawn(|| index_runtime(shard1, recv_chan))?;
 
     match server_threads.join() {
-        Ok(_) => "Server thread has exited safely - shutting down",
-        Err(_) => "Server thread has crashed - restart application",
+        Ok(_) => event!(Level::INFO, message="Server thread has exited safely - shutting down"),
+        Err(_) => event!(Level::ERROR, message="Server thread has crashed - restart application"),
     };
     processor_thread.join()?;
     /*
@@ -74,27 +87,31 @@ impl QueryShard {
     }
 }
 
+fn form_path(query_id: u64, doc_id: u64) -> PathBuf {
+    let mut path = PathBuf::from(DATA_PATH);
+    let subpath = PathBuf::from(format!("{query_id}"));
+    path.push(subpath);
+    path.push(format!("{doc_id}.rkyv"));
+    path
+}
+
 async fn index_runtime(shard: QueryShard, mut text_recv: tachyonix::Receiver<TextSource>) {
     loop {
         if let Ok(doc) = text_recv.recv().await {
+            event!(Level::INFO, message="document received", thread_id=glommio::executor().id(), ?doc.id, ?doc.name);
+            let doc_id=doc.id;
             let mut search_results = shard.search(doc).await;
             for index_data in search_results.drain(0..) {
                 glommio::spawn_local(async move {
-                    let path = format!(
-                        "/home/fridgeseal/Projects/Tarkine/output_data/{}_{}_{}.rkyv",
-                        shard.id, index_data.source_query, index_data.document_id
-                    );
-                    let output_path = Path::new(&path);
-                    let mut sink = glommio::io::ImmutableFileBuilder::new(output_path)
-                        .build_sink()
-                        .await
-                        .unwrap();
-                    let index_buffer = rkyv::to_bytes::<_, 1024>(&index_data).unwrap();
-                    sink.write(index_buffer.as_slice())
-                        .await
-                        .expect("Couldn't write buffer");
-                    sink.seal().await.expect("Couldn't close seal");
-                    println!("Wrote to file!");
+                    let output_path = form_path( index_data.source_query, index_data.document_id);
+                    let debug_path = output_path.clone(); // fight me
+                    defer!(event!(Level::INFO, message="Wrote results into file!", thread_id=glommio::executor().id(), ?doc_id, file_path=?debug_path));
+                    std::fs::create_dir_all(output_path.parent().unwrap()).expect("Couldn't create dir manually"); // yay std lib functions
+                    let dma_file = glommio::io::DmaFile::create(output_path).await.expect("Couldn't create file");
+                    let mut writer = DmaStreamWriterBuilder::new(dma_file).build();
+                    let index_buffer = rkyv::to_bytes::<_, 1024>(&index_data).unwrap(); // Live dangerously
+                    writer.write_all(index_buffer.as_slice()).await.expect("Couldn't write file!");
+                
                 })
                 .await;
             }
