8885c3e26ea266f4eb67a84f17a8a4f3c0450b3f
Tom Watson <tom_w@fastmail.com>
2022-11-06 22:21:55 +1100

    Move shared structures into lib module

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,71 @@
-use tracing_subscriber::{fmt::format::FmtSpan, prelude::*};
+// use crate::{errors::ApiError, search::TextSource};
+
+use bytecheck::CheckBytes;
+use rkyv::{Archive, Deserialize, Serialize};
+
+use tracing_subscriber::prelude::*;
 
 #[tarpc::service]
-pub trait World {
+pub trait Splinter {
     async fn hello(name: String) -> String;
-    async fn healthcheck() -> &'static str;
+    async fn healthcheck() -> String;
+    async fn peer_health_capacity() -> LoadCapacityData;
+    async fn get_query(query_id: u64) -> PersistentQuery;
+    async fn submit_query(query: PersistentQuery);
 }
 
 pub fn init_tracing(service_name: &str) -> anyhow::Result<()> {
     tracing_subscriber::registry()
-    .with(tracing_subscriber::EnvFilter::new(
+        .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG")
-            .unwrap_or_else(|_| "splinter=debug,tower_http=debug".into()),
-    ))
-    .with(tracing_subscriber::fmt::layer())
-    .init();
+                .unwrap_or_else(|_| format!("{service_name}=debug,tower_http=debug")),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     Ok(())
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct LoadCapacityData {
+    pub conn_count: u32,
+    pub query_count: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, serde::Serialize, serde::Deserialize)]
+pub struct PersistentQuery {
+    pub name: String,
+    pub query: String, // the query the user makes - later this should be a dedicated structure for more complex query types
+    pub id: u64,       // prefix/namespace to store stuff in database
+    pub score_threshold: i64,
+    result_count: u32,
+}
+
+impl PersistentQuery {
+    pub fn new(id: u64, name: impl Into<String>, q: impl Into<String>, threshold: i64) -> Self {
+        Self {
+            name: name.into(),
+            query: q.into(),
+            id,
+            score_threshold: threshold, // Need a good way of refining this
+            result_count: 0,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Archive, Deserialize, Serialize, serde::Serialize)]
+#[archive_attr(derive(CheckBytes, Debug))]
+pub struct IndexData {
+    /// Contains all necessary information to add a document to a query's results
+    pub source_query: u64,
+    pub key: u64,
+    pub document_id: u64,
+    pub name: String,
+    pub match_indices: Vec<[usize; 2]>,
+    pub score: i64,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct MatchData {
+    snippet: String,
+    score: u32,
 }
