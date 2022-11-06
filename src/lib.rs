ec4a5abe264ddd4e3bae4ec8c8f55dfa7dc2ff10
Tom Watson <tom_w@fastmail.com>
2022-11-06 23:49:41 +1100

    Implement zero-copy get_query via rkyv + sled

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 // use crate::{errors::ApiError, search::TextSource};
 
 use bytecheck::CheckBytes;
-use rkyv::{Archive, Deserialize, Serialize};
+use rkyv::{validation::CheckTypeError, Archive, Deserialize, Serialize};
 
+use thiserror::Error;
 use tracing_subscriber::prelude::*;
 
 #[tarpc::service]
@@ -10,7 +11,7 @@ pub trait Splinter {
     async fn hello(name: String) -> String;
     async fn healthcheck() -> String;
     async fn peer_health_capacity() -> LoadCapacityData;
-    async fn get_query(query_id: u64) -> PersistentQuery;
+    async fn get_query(query_id: u64) -> Result<PersistentQuery, TarkineError>;
     async fn submit_query(query: PersistentQuery);
     async fn submit_document(document: TextSource);
     async fn get_results(query_id: u64) -> Vec<IndexData>;
@@ -33,7 +34,22 @@ pub struct LoadCapacityData {
     pub query_count: u32,
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(
+    Archive,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash,
+    Clone,
+    serde::Serialize,
+    serde::Deserialize,
+    Serialize,
+    Deserialize,
+)]
+// This will generate a PartialEq impl between our unarchived and archived types
+#[archive(compare(PartialEq))]
+// To use the safe API, you have to derive CheckBytes for the archived type
+#[archive_attr(derive(CheckBytes, Debug))]
 pub struct PersistentQuery {
     pub name: String,
     pub query: String, // the query the user makes - later this should be a dedicated structure for more complex query types
@@ -100,4 +116,23 @@ impl TextSource {
     }
 
     // Lazy loading from supported sources, etc
+}
+
+#[tarpc::derive_serde]
+#[derive(Debug, Error)]
+pub enum TarkineError {
+    #[error("Error reading/writing from storage layer")]
+    Storage,
+    #[error("Error communicating over network")]
+    Network,
+    #[error("Id not found or not correct format")]
+    Id,
+    #[error("Could not parse bytes")]
+    Parsing,
+}
+
+impl From<sled::Error> for TarkineError {
+    fn from(_: sled::Error) -> Self {
+        Self::Storage
+    }
 }
